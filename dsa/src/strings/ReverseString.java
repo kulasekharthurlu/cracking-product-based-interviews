7a4946f9a520e203f58d0f09c74ab1d4efdc6369
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2025-12-16 16:45:05 +0530

    Reverse string using recursion

---
+++ dsa/src/strings/ReverseString.java
@@ -0,0 +1,14 @@
+package strings;
+
+public class ReverseString {
+    public static String reverse(String str) {
+        if (str.isEmpty()) {
+            return str;
+        }
+        return reverse(str.substring(1)) + str.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse("hello"));
+    }
+}
