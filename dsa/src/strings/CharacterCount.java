013a9cc3609c7edc238fc08bfcab7b41ffd660f6
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2025-12-20 21:26:33 +0530

    Count the characters in the string

---
+++ dsa/src/strings/CharacterCount.java
@@ -0,0 +1,25 @@
+package strings;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CharacterCount {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder();
+
+        Arrays.stream("aaabbcc".split(""))
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        LinkedHashMap::new,    
+                        Collectors.counting()
+                ))
+                .forEach((key, value) ->
+                        sb.append(key).append(value)
+                );
+
+        System.out.println(sb.toString()); // a3b2c2
+    }
+}
