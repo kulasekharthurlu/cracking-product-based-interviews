7a50b92e1f6c5730bfdfddcf6b554c407b137317
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2025-12-22 09:38:47 +0530

    Majority element in Array

---
+++ dsa/src/arrays/MajorityElement.java
@@ -0,0 +1,37 @@
+package arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+
+    public static int findMajorityElement(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return -1;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+            // Early exit when majority is found
+            if (map.get(num) > arr.length / 2) {
+                return num;
+            }
+        }
+        return -1; // No majority element
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 2, 1, 2, 3, 2, 2};
+
+        int result = findMajorityElement(arr);
+
+        if (result != -1) {
+            System.out.println("Majority Element: " + result);
+        } else {
+            System.out.println("No Majority Element found");
+        }
+    }
+}
