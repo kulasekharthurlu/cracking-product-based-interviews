4138d75b9c1cb42da86cb0a0b18a2f51c9ba741a
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-02-20 16:59:20 +0530

    Reverse array with two pointer approch

---
+++ dsa/src/top_patterns/two_pointers/Arrays.java
@@ -0,0 +1,13 @@
+
+public class Arrays{
+    public int[] reverseArray(int[] arr){
+       int start = 0;
+       int end = arr.length-1;
+       while(start < end){
+          int temp = arr[end];
+          arr[end--] = arr[start];
+          arr[start++] = temp;
+       }
+       return arr;
+    }
+}
