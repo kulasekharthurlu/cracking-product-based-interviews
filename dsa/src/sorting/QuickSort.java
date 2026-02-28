cee27e504c60f4758908cffddd1bae627bcf3e75
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-02-28 21:26:32 +0530

    Implemented quick sort

---
+++ dsa/src/sorting/QuickSort.java
@@ -0,0 +1,37 @@
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+	
+	public void sort(int[] arr,int low,int high) {
+		if(low<high) {
+			int p=partition(arr, low, high);
+			sort(arr,low,p-1);
+			sort(arr,p+1,high);
+		}
+	}
+	
+	public int partition(int[] arr, int low, int high) {
+		int povote = arr[high];
+		int i = low;
+		int j = low;
+		while (i <= high) {
+			if (arr[i] <= povote) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+				j++;
+			}
+			i++;
+		}
+		return j - 1;
+	}
+	public static void main(String[] args) {
+		int[] arr= {1,3,5,2,9,7};
+		QuickSort qs=new QuickSort();
+		qs.sort(arr, 0, arr.length-1);
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
