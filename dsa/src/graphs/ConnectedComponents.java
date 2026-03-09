40a054f46836a4e1fb46d6dcc3542c8f4ee556f6
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-03-09 21:48:11 +0530

    Implemented dfs in graph

---
+++ dsa/src/graphs/ConnectedComponents.java
@@ -0,0 +1,29 @@
+import java.util.LinkedList;
+
+public class ConnectedComponents {
+int V=6;
+LinkedList<Integer>[] adj;
+	
+	public void dfs() {
+		boolean[] visited=new boolean[V];
+		int[] compId=new int[V];
+		int count=0;
+		for(int v=0;v<V;v++) {
+			if(!visited[v]) {
+				dfs(v,visited,compId,count);
+				count++;
+			}
+		}
+		
+	}
+	
+	public void dfs(int v,boolean[] visited,int[] compId,int count ) {
+		visited[v]=true;
+		compId[v]=count;
+		for(int w:adj[v]) {
+			if(!visited[w]) {
+				dfs(w,visited,compId,count);
+			}
+		}
+	}
+}
