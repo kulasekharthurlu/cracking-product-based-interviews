004344bc74cf00e65c3007d5ae781351a508651a
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-03-18 21:56:04 +0530

    Squares of sorted array with two pointer

---
+++ dsa/src/top_patterns/two_pointers/SquaresOfSortedArray.java
@@ -0,0 +1,22 @@
+public class SquaresOfSortedArray{
+    public int[] sortedSquares(int[] nums) {
+    int left = 0, right = nums.length - 1;
+    int[] result = new int[nums.length];
+    int index = nums.length - 1;
+
+    while (left <= right) {
+        int l = nums[left] * nums[left];
+        int r = nums[right] * nums[right];
+
+        if (l > r) {
+            result[index--] = l;
+            left++;
+        } else {
+            result[index--] = r;
+            right--;
+        }
+    }
+    return result;
+}
+
+}
