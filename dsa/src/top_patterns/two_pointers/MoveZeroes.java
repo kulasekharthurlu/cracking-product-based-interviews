8f93c79472e1200998eed029a52de5962a616c91
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-03-19 18:58:53 +0530

    Move zeros with two pointers

---
+++ dsa/src/top_patterns/two_pointers/MoveZeroes.java
@@ -0,0 +1,14 @@
+public class MoveZeros{
+    public void moveZeroes(int[] nums) {
+    int slow = 0;
+
+    for (int fast = 0; fast < nums.length; fast++) {
+        if (nums[fast] != 0) {
+            int temp = nums[slow];
+            nums[slow] = nums[fast];
+            nums[fast] = temp;
+            slow++;
+        }
+    }
+}
+}
