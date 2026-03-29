4a98e421434eb64f2ef7cb9665b979d429a3b5d7
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-03-29 21:31:41 +0530

    Create Longest Subarray with Sum ≤ K with sliding window

---
+++ dsa/src/top_patterns/sliding_window/LongestSubarrayWithSumLessThanK.java
@@ -0,0 +1,20 @@
+public class LongestSubarrayWithSumLessThanK{
+
+public int longestSubarray(int[] nums, int k) {
+    int left = 0, sum = 0, maxLen = 0;
+
+    for (int right = 0; right < nums.length; right++) {
+        sum += nums[right];
+
+        while (sum > k) {
+            sum -= nums[left];
+            left++;
+        }
+
+        maxLen = Math.max(maxLen, right - left + 1);
+    }
+    return maxLen;
+}
+
+
+}
