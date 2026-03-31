099ed405650f8f3c368c71cac02442c11cb4abc5
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-03-31 21:45:47 +0530

    Maximum Number of Vowels in Substring of Size K with sliding window

---
+++ dsa/src/top_patterns/sliding_window/MaximumNumberOfVowels.java
@@ -0,0 +1,18 @@
+public class MaximumNumberOfVowels{
+public int maxVowels(String s, int k) {
+    int count = 0, max = 0;
+
+    for (int i = 0; i < k; i++) {
+        if ("aeiou".indexOf(s.charAt(i)) != -1) count++;
+    }
+    max = count;
+
+    for (int i = k; i < s.length(); i++) {
+        if ("aeiou".indexOf(s.charAt(i)) != -1) count++;
+        if ("aeiou".indexOf(s.charAt(i - k)) != -1) count--;
+        max = Math.max(max, count);
+    }
+    return max;
+}
+
+}
