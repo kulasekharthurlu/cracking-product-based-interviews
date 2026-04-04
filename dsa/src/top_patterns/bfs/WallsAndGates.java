13e7b98a16d80ad7c4f767e56986fed3a46921b7
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-04-04 20:28:55 +0530

    Created Walls and Gates with bfs

---
+++ dsa/src/top_patterns/bfs/WallsAndGates.java
@@ -0,0 +1,27 @@
+public class WallsAndGates{
+public void wallsAndGates(int[][] rooms) {
+    Queue<int[]> q = new LinkedList<>();
+
+    for (int i = 0; i < rooms.length; i++) {
+        for (int j = 0; j < rooms[0].length; j++) {
+            if (rooms[i][j] == 0) q.offer(new int[]{i, j});
+        }
+    }
+
+    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    while (!q.isEmpty()) {
+        int[] cur = q.poll();
+
+        for (int[] d : dirs) {
+            int x = cur[0] + d[0];
+            int y = cur[1] + d[1];
+
+            if (x >= 0 && y >= 0 && x < rooms.length && y < rooms[0].length && rooms[x][y] == Integer.MAX_VALUE) {
+                rooms[x][y] = rooms[cur[0]][cur[1]] + 1;
+                q.offer(new int[]{x, y});
+            }
+        }
+    }
+}
+}
