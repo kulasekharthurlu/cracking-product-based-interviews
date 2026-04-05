679450e79b143d5fe7d9543a2aab45b4c2bcba1f
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-04-05 23:15:33 +0530

    Created Rotting Oranges with bfs

---
+++ dsa/src/top_patterns/bfs/RottingOranges.java
@@ -0,0 +1,38 @@
+public class RottingOranges{
+public int orangesRotting(int[][] grid) {
+    Queue<int[]> q = new LinkedList<>();
+    int fresh = 0;
+
+    for (int i = 0; i < grid.length; i++) {
+        for (int j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] == 2) q.offer(new int[]{i, j});
+            if (grid[i][j] == 1) fresh++;
+        }
+    }
+
+    int time = 0;
+    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    while (!q.isEmpty() && fresh > 0) {
+        int size = q.size();
+        time++;
+
+        for (int i = 0; i < size; i++) {
+            int[] cur = q.poll();
+
+            for (int[] d : dirs) {
+                int x = cur[0] + d[0];
+                int y = cur[1] + d[1];
+
+                if (x >= 0 && y >= 0 && x < grid.length && y < grid[0].length && grid[x][y] == 1) {
+                    grid[x][y] = 2;
+                    fresh--;
+                    q.offer(new int[]{x, y});
+                }
+            }
+        }
+    }
+    return fresh == 0 ? time : -1;
+}
+
+}
