5cca52bd50b05415048d4e13a7d170cba221d63f
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-04-16 23:01:28 +0530

    Created Happy Number with fast and slow pointers

---
+++ dsa/src/top_patterns/fast_and_slow_pointers/HappyNumber.java
@@ -0,0 +1,23 @@
+public class HappyNumber{
+public boolean isHappy(int n) {
+    int slow = n, fast = n;
+
+    do {
+        slow = squareSum(slow);
+        fast = squareSum(squareSum(fast));
+    } while (slow != fast);
+
+    return slow == 1;
+}
+
+private int squareSum(int n) {
+    int sum = 0;
+    while (n > 0) {
+        int d = n % 10;
+        sum += d * d;
+        n /= 10;
+    }
+    return sum;
+}
+
+}
