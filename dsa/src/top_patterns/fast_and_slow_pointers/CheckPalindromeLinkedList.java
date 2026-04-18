7e3876d238443a0e417c1ef42821a2c8bdc16a77
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-04-18 21:06:51 +0530

    Created check palindrome linked list with fast and slow pointers

---
+++ dsa/src/top_patterns/fast_and_slow_pointers/CheckPalindromeLinkedList.java
@@ -0,0 +1,29 @@
+public class CheckPalindromeLinkedList{
+public boolean isPalindrome(ListNode head) {
+    ListNode slow = head, fast = head;
+
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    // reverse second half
+    ListNode prev = null;
+    while (slow != null) {
+        ListNode next = slow.next;
+        slow.next = prev;
+        prev = slow;
+        slow = next;
+    }
+
+    // compare
+    ListNode left = head, right = prev;
+    while (right != null) {
+        if (left.val != right.val) return false;
+        left = left.next;
+        right = right.next;
+    }
+    return true;
+}
+
+}
