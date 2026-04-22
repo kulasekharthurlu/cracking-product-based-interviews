76fefdd67f49440c1e244c45bd73b8a127bd6873
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-04-22 23:05:39 +0530

    Find start of cycle with fast and slow poiters

---
+++ dsa/src/top_patterns/fast_and_slow_pointers/FindStartOfCycle.java
@@ -0,0 +1,23 @@
+public class FindStartOfCycle{
+public ListNode detectCycle(ListNode head) {
+    ListNode slow = head, fast = head;
+
+    // Detect cycle
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow == fast) {
+            slow = head;
+
+            while (slow != fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
+        }
+    }
+    return null;
+}
+
+}
