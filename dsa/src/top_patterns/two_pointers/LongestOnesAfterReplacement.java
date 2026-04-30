e9703d130c0cfdbc5bc95d75d742e2ae755fe948
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-04-30 23:10:13 +0530

    Longest ones after replacement with two pointers

---
+++ dsa/src/top_patterns/two_pointers/LongestOnesAfterReplacement.java
@@ -0,0 +1,16 @@
+public class LongestOnesAfterReplacement{
+public int longestOnes(int[] nums, int k) {
+    int left = 0, zero = 0, max = 0;
+
+    for (int right = 0; right < nums.length; right++) {
+        if (nums[right] == 0) zero++;
+
+        while (zero > k) {
+            if (nums[left++] == 0) zero--;
+        }
+        max = Math.max(max, right - left + 1);
+    }
+    return max;
+}
+
+}
