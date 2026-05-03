670e63276a30a63c05f9fe9dbd3f8c708c9e769c
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-05-03 22:46:36 +0530

    Top view of the tree

--- dsa/src/trees/Trees.java
+++ dsa/src/trees/Trees.java
@@ -92,6 +92,34 @@ public List<Integer> bottomView(TreeNode root) {
 }
 
 
+public List<Integer> topView(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    if (root == null) return res;
+
+    Map<Integer, Integer> map = new TreeMap<>();
+    Queue<Pair> q = new LinkedList<>();
+
+    q.offer(new Pair(root, 0));
+
+    while (!q.isEmpty()) {
+        Pair p = q.poll();
+
+        if (!map.containsKey(p.hd)) {
+            map.put(p.hd, p.node.val);
+        }
+
+        if (p.node.left != null)
+            q.offer(new Pair(p.node.left, p.hd - 1));
+
+        if (p.node.right != null)
+            q.offer(new Pair(p.node.right, p.hd + 1));
+    }
+
+    res.addAll(map.values());
+    return res;
+}
+
+
 }
 
 class TreeNode{
