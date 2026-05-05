f5721bc1ed14209d7d982c529e1b31badaf75cfc
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-05-05 22:55:23 +0530

    Check polindrom with recursion

---
+++ dsa/src/recursion/CheckPolindrom.java
@@ -0,0 +1,7 @@
+public class CheckPolindrom{
+public boolean isPalindrome(String s, int l, int r) {
+    if (l >= r) return true;
+    if (s.charAt(l) != s.charAt(r)) return false;
+    return isPalindrome(s, l + 1, r - 1);
+}
+}
