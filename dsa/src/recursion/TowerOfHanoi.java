d564c02ae1ac5900a89ec626804510e7ee693e55
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-05-07 23:07:06 +0530

    Tower of anoi with recursion

---
+++ dsa/src/recursion/TowerOfHanoi.java
@@ -0,0 +1,12 @@
+public class TowerOfHanoi{
+public void hanoi(int n, char src, char aux, char dest) {
+    if (n == 1) {
+        System.out.println(src + " -> " + dest);
+        return;
+    }
+
+    hanoi(n - 1, src, dest, aux);
+    System.out.println(src + " -> " + dest);
+    hanoi(n - 1, aux, src, dest);
+}
+}
