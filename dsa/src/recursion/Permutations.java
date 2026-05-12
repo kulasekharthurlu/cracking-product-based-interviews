ee97760c90de57cf3957af0ee2deb7281d02c4e1
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-05-12 22:52:11 +0530

    Permutations with recursion

---
+++ dsa/src/recursion/Permutations.java
@@ -0,0 +1,22 @@
+public class Permutations{
+public void permute(int[] nums, int start, List<List<Integer>> res) {
+    if (start == nums.length) {
+        List<Integer> list = new ArrayList<>();
+        for (int n : nums) list.add(n);
+        res.add(list);
+        return;
+    }
+
+    for (int i = start; i < nums.length; i++) {
+        swap(nums, i, start);
+        permute(nums, start + 1, res);
+        swap(nums, i, start);
+    }
+}
+
+private void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+}
