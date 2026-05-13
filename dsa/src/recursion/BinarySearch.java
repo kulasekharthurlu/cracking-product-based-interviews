ce2f9071381fb8c0b5385d4e2ed378e5412cb0f6
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-05-13 22:45:45 +0530

    Binary search with recursion

---
+++ dsa/src/recursion/BinarySearch.java
@@ -0,0 +1,13 @@
+public class BinarySearch{
+public int binarySearch(int[] arr, int left, int right, int target) {
+    if (left > right) return -1;
+
+    int mid = left + (right - left) / 2;
+
+    if (arr[mid] == target) return mid;
+    else if (arr[mid] > target)
+        return binarySearch(arr, left, mid - 1, target);
+    else
+        return binarySearch(arr, mid + 1, right, target);
+}
+}
