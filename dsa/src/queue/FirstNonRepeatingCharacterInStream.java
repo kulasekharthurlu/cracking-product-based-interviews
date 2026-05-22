36b5ad3ddfee5f989cddc82c21bb6d77d426523f
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-05-22 22:20:57 +0530

    First Non-Repeating Character in Stream with queue

---
+++ dsa/src/queue/FirstNonRepeatingCharacterInStream.java
@@ -0,0 +1,23 @@
+public class FirstNonRepeatingCharacterInStream{
+public String firstNonRepeating(String s) {
+    Queue<Character> q = new LinkedList<>();
+    int[] freq = new int[26];
+
+    StringBuilder sb = new StringBuilder();
+
+    for (char c : s.toCharArray()) {
+
+        freq[c - 'a']++;
+        q.offer(c);
+
+        while (!q.isEmpty() && freq[q.peek() - 'a'] > 1) {
+            q.poll();
+        }
+
+        if (q.isEmpty()) sb.append('#');
+        else sb.append(q.peek());
+    }
+
+    return sb.toString();
+}
+}
