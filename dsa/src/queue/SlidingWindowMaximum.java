69c5430b9921ecaa7b1bdd19c97c2cb51023f9a2
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-05-23 22:37:09 +0530

    Sliding window maximum with queue

---
+++ dsa/src/queue/SlidingWindowMaximum.java
@@ -0,0 +1,25 @@
+public class SlidingWindowMaximum{
+public int[] maxSlidingWindow(int[] nums, int k) {
+
+    Deque<Integer> dq = new LinkedList<>();
+    int[] res = new int[nums.length - k + 1];
+
+    int idx = 0;
+
+    for (int i = 0; i < nums.length; i++) {
+
+        while (!dq.isEmpty() && dq.peekFirst() <= i - k)
+            dq.pollFirst();
+
+        while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i])
+            dq.pollLast();
+
+        dq.offerLast(i);
+
+        if (i >= k - 1)
+            res[idx++] = nums[dq.peekFirst()];
+    }
+
+    return res;
+}
+}
