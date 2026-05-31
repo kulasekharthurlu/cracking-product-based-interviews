218654c73f0948951cc28f72ccf743c82ab7b524
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-05-31 23:12:58 +0530

    Stock span problem with stack

---
+++ dsa/src/stack/StockSpanProblem.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+public class StockSpan {
+
+    public static int[] calculateSpan(int[] price) {
+
+        int n = price.length;
+        int[] span = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(0);
+        span[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+
+            while (!stack.isEmpty() &&
+                    price[stack.peek()] <= price[i]) {
+
+                stack.pop();
+            }
+
+            span[i] =
+                    stack.isEmpty()
+                    ? i + 1
+                    : i - stack.peek();
+
+            stack.push(i);
+        }
+
+        return span;
+    }
+}
