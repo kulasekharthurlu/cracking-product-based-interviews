18179bb50d0535289776343a96baccd55629c58c
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-06-12 20:06:55 +0530

    Created remove outer parentheses with stack

---
+++ dsa/src/stack/RemoveOuterParentheses.java
@@ -0,0 +1,43 @@
+public class RemoveOuterParentheses {
+
+    public String removeOuterParentheses(String s) {
+
+        StringBuilder result = new StringBuilder();
+        int depth = 0;
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(') {
+
+                if (depth > 0) {
+                    result.append(ch);
+                }
+
+                depth++;
+
+            } else {
+
+                depth--;
+
+                if (depth > 0) {
+                    result.append(ch);
+                }
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+
+        RemoveOuterParentheses solution =
+                new RemoveOuterParentheses();
+
+        String input = "(()())(())";
+        String output =
+                solution.removeOuterParentheses(input);
+
+        System.out.println("Input  : " + input);
+        System.out.println("Output : " + output);
+    }
+}
