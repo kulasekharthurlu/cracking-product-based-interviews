d0e8ab56ddff1d00a7d00530fe7ff6b991af80a0
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-06-13 21:46:12 +0530

    Created longest consecutive sequence with hashtable

---
+++ dsa/src/hashtable/LongestConsecutiveSequence.java
@@ -0,0 +1,30 @@
+public class LongestConsecutiveSequence{
+public int longestConsecutive(int[] nums) {
+
+    Set<Integer> set = new HashSet<>();
+
+    for (int num : nums) {
+        set.add(num);
+    }
+
+    int longest = 0;
+
+    for (int num : set) {
+
+        if (!set.contains(num - 1)) {
+
+            int current = num;
+            int streak = 1;
+
+            while (set.contains(current + 1)) {
+                current++;
+                streak++;
+            }
+
+            longest = Math.max(longest, streak);
+        }
+    }
+
+    return longest;
+}
+}
