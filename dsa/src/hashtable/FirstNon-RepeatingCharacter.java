6e4b693251e67734645b50bd16e3ae9d06da7d10
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-06-16 22:00:13 +0530

    Created FirstNon-RepeatingCharacter with hashmap

---
+++ dsa/src/hashtable/FirstNon-RepeatingCharacter.java
@@ -0,0 +1,19 @@
+public class FirstNonRepeatingCharacter{
+public char firstNonRepeating(String s) {
+
+    Map<Character, Integer> map = new HashMap<>();
+
+    for (char c : s.toCharArray()) {
+        map.put(c, map.getOrDefault(c, 0) + 1);
+    }
+
+    for (char c : s.toCharArray()) {
+
+        if (map.get(c) == 1) {
+            return c;
+        }
+    }
+
+    return '#';
+}
+}
