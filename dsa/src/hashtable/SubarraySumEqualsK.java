762e4932cdd41242b91ce2f79938a5fb4166fdb2
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-06-19 22:59:53 +0530

    Created SubarraySumEqualsK.java with

---
+++ dsa/src/hashtable/SubarraySumEqualsK.java
@@ -0,0 +1,25 @@
+public class SubarraySumEqualsK{
+public int subarraySum(int[] nums, int k) {
+
+    Map<Integer, Integer> map = new HashMap<>();
+
+    map.put(0, 1);
+
+    int sum = 0;
+    int count = 0;
+
+    for (int num : nums) {
+
+        sum += num;
+
+        if (map.containsKey(sum - k)) {
+            count += map.get(sum - k);
+        }
+
+        map.put(sum,
+                map.getOrDefault(sum, 0) + 1);
+    }
+
+    return count;
+}
+}
