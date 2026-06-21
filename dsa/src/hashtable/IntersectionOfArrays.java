6098d98d81440e62466ebc5a657363fd63f09392
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-06-21 22:52:44 +0530

    Created Intersection of arrays with hashmap

---
+++ dsa/src/hashtable/IntersectionOfArrays.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class IntersectionOfArrays {
+
+    public static int[] intersection(int[] nums1, int[] nums2) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums1) {
+            map.put(num, 1);
+        }
+
+        Set<Integer> resultSet = new HashSet<>();
+
+        for (int num : nums2) {
+
+            if (map.containsKey(num)) {
+                resultSet.add(num);
+            }
+        }
+
+        int[] result = new int[resultSet.size()];
+        int index = 0;
+
+        for (int num : resultSet) {
+            result[index++] = num;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2};
+
+        System.out.println(
+                Arrays.toString(intersection(nums1, nums2))
+        );
+    }
+}
