2581beafb8e9eefc4beb5a484bcc036f49045481
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-06-27 22:46:58 +0530

    Created Number of recent calls with queue

---
+++ dsa/src/queue/NumberOfRecentCalls.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class RecentCounter {
+
+    Queue<Integer> queue = new LinkedList<>();
+
+    public int ping(int t) {
+
+        queue.offer(t);
+
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+
+        return queue.size();
+    }
+}
