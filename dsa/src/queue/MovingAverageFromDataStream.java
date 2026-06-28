9a18c6b5e2c618035c5368b643c7e5b4e404b839
Kulasekhar <74496768+kulasekharthurlu@users.noreply.github.com>
2026-06-28 22:24:42 +0530

    Created moving average from data stream with queue

---
+++ dsa/src/queue/MovingAverageFromDataStream.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MovingAverage {
+
+    private Queue<Integer> queue;
+    private int size;
+    private double sum;
+
+    public MovingAverage(int size) {
+        this.size = size;
+        queue = new LinkedList<>();
+        sum = 0;
+    }
+
+    public double next(int val) {
+
+        queue.offer(val);
+        sum += val;
+
+        if (queue.size() > size) {
+            sum -= queue.poll();
+        }
+
+        return sum / queue.size();
+    }
+}
